1586b0b745dde1ca234297826519d9d0be51bfc2
tenuckii <152208465+tenuckii@users.noreply.github.com>
2025-06-12 06:47:38 -0400

    center canvas

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -58,10 +58,21 @@ export default class Canvas {
 		this.canvas.style.transform = `translate(${this.translateX - deltaX}px, ${this.translateY - deltaY}px) scale(${this.scale})`;
 	}
 
+	private updateTransformScale(scale: number) {
+		this.scale = scale;
+		this.canvas.style.transform = `translate(${this.translateX}px, ${this.translateY}px) scale(${this.scale})`;
+	}
+	public updateTransformAll(deltaX: number, deltaY: number,scale: number) {
+		this.translateX = deltaX;
+		this.translateY = deltaY;
+		this.scale = scale;
+		this.canvas.style.transform = `translate(${this.translateX}px, ${this.translateY}px) scale(${this.scale})`;
+	}
+
 	public ctrlZoom(deltaY: number) {
 		const delta = deltaY > 0 ? -0.1 : 0.1;
 		this.scale += delta;
-		this.scale = Math.max(0.1, Math.min(this.scale, 5));
+		this.scale = Math.max(0.1, Math.min(this.scale, 100));
 		this.updateTransform();
 	}
 
